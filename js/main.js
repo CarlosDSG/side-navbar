984af1a9b37b295d0c6087d57facab5468b679cf
Carlos D'Santiago <carlosdavidsantiago4@gmail.com>
2022-06-21 21:04:44 -0400

    añadiendo overlay

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
+const overlay = document.querySelector('.overlay');
+
 function openNav() {
     document.getElementById('mySidenav').style.width = '250px';
     document.getElementById('main').style.marginRight = '250px';
     document.querySelector('span').style.visibility = 'hidden';
-    /* document.querySelector('body').style.filter = 'brightness(31%)'; */
+    overlay.classList.toggle('visible');
 }
 
 function closeNav() {
     document.getElementById('mySidenav').style.width = '0';
     document.getElementById('main').style.marginRight = '0';
     document.querySelector('span').style.visibility = 'visible';
+    overlay.classList.toggle('visible');
 }
